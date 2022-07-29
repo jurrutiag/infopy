8a446fa4ab8c5ad38cf7f1649d86a39552744850
Juan Urrutia <juan.urrutia.gandolfo@gmail.com>
2022-07-29 11:48:55 -0400

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+
+from glob import glob
 from os import path
+from os.path import splitext
+from os.path import basename
 
 from setuptools import find_packages
 from setuptools import setup
@@ -16,13 +20,28 @@ setup(
     url="https://github.com/jurrutiag/infopy",
     author="Juan Urrutia",
     author_email="juan.urrutia.gandolfo@gmail.com",
-    license="MIT",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
+    zip_safe=False,
     classifiers=[
-        "License :: OSI Approved :: MIT License",
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    packages=find_packages(),
     install_requires=["numpy", "scipy", "scikit-learn"],
-    extras_requires=[],
+    extras_require={
+        "testing": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "tox>=3.24",
+        ]
+    },
 )
